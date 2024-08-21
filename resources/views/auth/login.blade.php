16b40e9159b18596f673e819eacafa34b1b6a030
afnan khan <afnanweb122@gmail.com>
2024-08-21 14:26:08 +0500

    login form added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,37 @@
             </div>
         </div>
     </div>
+</div> --}}
+<div id="page-content" class="page-content d-flex align-items-center justify-content-center vh-100" style="background-image: url('assests/img/bg-header.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <div class="card card-login mx-auto" style="max-width: 400px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+        <div class="card-body">
+            <h2 class="text-center mb-4">Login</h2>
+            <form class="form-horizontal" action="index.html">
+                <div class="form-group row">
+                    <div class="col-md-20">
+                        <input class="form-control" type="text" required="" placeholder="Username">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-20">
+                        <input class="form-control" type="password" required="" placeholder="Password">
+                    </div>
+                </div>
+                <div class="form-group row d-flex justify-content-between">
+                    <div class="col-md-12 text-right">
+                        <a href="#" class="text-primary">Forgot password?</a>
+                    </div>
+                </div>
+                <div class="form-group row text-center mt-4">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary btn-block text-uppercase">Log In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
+
+
 @endsection

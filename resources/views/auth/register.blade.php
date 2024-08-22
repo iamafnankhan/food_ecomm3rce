63dc41cbac3930f5752a8f37201f88380094a227
afnan khan <afnanweb122@gmail.com>
2024-08-22 11:47:02 +0500

    register form done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -96,6 +96,7 @@
                     <div class="card card-login mb-5">
                         <div class="card-body">
                             <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                                @csrf
                                 <div class="form-group row mt-3">
                                     <div class="col-md-12">
                                         <input placeholder="Name" id="name" type="text"
@@ -122,7 +123,7 @@
                                         @enderror
                                     </div>
                                 </div>
-
+{{-- 
                                 <div class="form-group row mt-3">
                                     <div class="col-md-12">
                                         <input placeholder="Username" id="username" type="text"
@@ -135,47 +136,53 @@
                                             </span>
                                         @enderror
                                     </div>
-                                </div>
-                                <div class="form-group row mt-3">
-                                    <div class="col-md-12">
-                                        <input placeholder="Password" id="password" type="password"
-                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                            value="{{ old('password') }}" required autocomplete="password" autofocus>
-
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                </div> --}}
+                                {{-- <form method="POST" action="{{ route('register') }}"> --}}
+                                    {{-- @csrf --}}
+                                    <div class="form-group row mt-3">
+                                        <div class="col-md-12">
+                                            <input placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-group row mt-3">
-                                    <div class="col-md-12">
-                                        <input placeholder="Confirm Password" id="confirm_password" type="password"
-                                            class="form-control @error('confirm_password') is-invalid @enderror"
-                                            name="confirm_password" value="{{ old('confirm_password') }}" required
-                                            autocomplete="new-password" autofocus>
-
-                                        @error('confirm_password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    {{-- <div class="form-group row mt-3">
+                                        <div class="col-md-12">
+                                            <input placeholder="confirm_password" id="confirm_password" type="password"
+                                                class="form-control @error('confirm_password') is-invalid @enderror"
+                                                name="confirm_password" value="{{ old('confirm_password') }}" required autofocus>
+                                
+                                            @error('confirm_password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div> --}}
+                                    <div class="form-group row mt-3">
+                                        <div class="col-md-12">
+                                            <button type="submit" class="btn btn-primary">
+                                                Submit
+                                            </button>
+                                        </div>
                                     </div>
-
-                                </div>
+                                </form>
+                                
                                 <div class="form-group row">
                                     <div class="col-md-12">
                                         <div class="checkbox">
                                             <input id="checkbox0" type="checkbox" name="terms">
                                             <label for="checkbox0" class="mb-0">I Agree with <a href="terms.html"
-                                                    class="text-light">Terms & Conditions</a> </label>
+                                                    class="text-light-black">Terms & Conditions</a> </label>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="form-group row text-center mt-4">
                                     <div class="col-md-12">
-                                        <button type="submit"
+                                        <button type="submit" name="submit"
                                             class="btn btn-primary btn-block text-uppercase">Register</button>
                                     </div>
                                 </div>
